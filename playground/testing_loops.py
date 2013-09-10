30b0f97688bb417f86fa4348b79c71bc40ee7a20
Alvin Svitzer <asvitzer@magnetstreet.com>
2013-09-10 15:33:32 -0500

    Testing out loops and list funtionality with python

---
+++ playground/testing_loops.py
@@ -0,0 +1,42 @@
+# Define a procedure, sum_list,
+# that takes as its input a
+# list of numbers, and returns
+# the sum of all the elements in
+# the input list.
+
+def sum_list(numList):
+    sum = 0
+    for x in numList:
+        sum = sum + x
+    return sum
+
+print sum_list([1, 7, 4])
+#>>> 12
+
+print sum_list([9, 4, 10])
+#>>> 23
+
+print sum_list([44, 14, 76])
+#>>> 134
+
+# Define a procedure, measure_udacity,
+# that takes as its input a list of strings,
+# and returns a number that is a count
+# of the number of elements in the input
+# list that start with the uppercase 
+# letter 'U'.
+
+def measure_udacity (stringList):
+    count = 0
+    for x in stringList:
+        if x.[0] == 'U':
+            count = count + 1
+    return count
+
+
+
+print measure_udacity(['Dave','Sebastian','Katy'])
+#>>> 0
+
+print measure_udacity(['Umika','Umberto'])
+#>>> 2
